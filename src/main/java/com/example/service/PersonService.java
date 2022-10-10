c5c7b326fe279052d8500bb46f2ba4a595a56ec2
yasser36 <115443645+yasser36@users.noreply.github.com>
2022-10-10 19:03:41 +0200

    update for testing

--- src/main/java/com/example/service/PersonService.java
+++ src/main/java/com/example/service/PersonService.java
@@ -40,6 +40,13 @@ public class PersonService {
 		p.setLastName("Mahmoud");
 		persons.add(p);
 		
+		p = new Person();
+		p.setId("5");
+		p.setAge(55);
+		p.setFirstName("web");
+		p.setLastName("test");
+		persons.add(p);
+		
 	}
 	public Person getPerson(String id) {
 		for(Person person:persons) {
